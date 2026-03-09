7f1f20036b8046d2ea8d40426513a61e101e76da
stephen <stephkno@gmail.com>
2026-03-08 20:47:31 -0700

    UART 300 baud clock timing issue at 48mhz

--- 4_uart/uart.v
+++ 4_uart/uart.v
@@ -1,5 +1,4 @@
 module uart (
-  // outputs
   output wire led_red,
   output wire led_green,
   output wire led_blue,
@@ -7,37 +6,40 @@ module uart (
   input wire serial_rxd
 );
 
-
   wire int_osc;
+  
   SB_HFOSC #(.CLKHF_DIV("0b00")) u_SB_HFOSC (.CLKHFPU(1'b1), .CLKHFEN(1'b1), .CLKHF(int_osc));
   
-  reg [31:0] pwm_counter;
-  reg [31:0] uart_baud_counter;
-  reg [3:0] tx_state;
-  reg [7:0] uart_data;
+  reg [31:0] pwm_counter = 0;
+  reg [31:0] uart_baud_counter = 160000;
+
+  reg [3:0] tx_state = 0;
+  reg [3:0] rx_state = 0;
+
+  reg [7:0] uart_data = 0;
+
+  reg rx_ready = 0;
 
   // color values
-  reg [7:0] red;
-  reg [7:0] green;
-  reg [7:0] blue;
+  reg [7:0] red = 255;
+  reg [7:0] green = 0;
+  reg [7:0] blue = 0;
 
-  initial begin
-    uart_data <= 0;
-  end
-  
+  // 0 = 300, 1 = 600, 2 = 1200, 5 = 9600, 17 = 2m
+  reg [4:0] baudrate = 0;
+
   always @(posedge int_osc) begin
     
     pwm_counter <= pwm_counter + 1;
-    uart_baud_counter <= uart_baud_counter + 1;
+    uart_baud_counter <= uart_baud_counter - 1;
 
     // 48,000,000 / 300 baud = 160000
-    if(uart_baud_counter == 160000)
+    if(uart_baud_counter == 0)
     begin
 
       // reset counter
-      uart_baud_counter <= 0;
+      uart_baud_counter <= 160000;
       
-      // send bit
       // 1 start bit + 8 data bits + 1 stop bit = 10 bits total
       if(tx_state == 0 || tx_state == 9) begin
         
@@ -45,22 +47,24 @@ module uart (
         serial_txd <= (tx_state == 9);
         
         if(tx_state == 9) begin
+
           tx_state <= 0;
 
+          // set output data:
           // print ascii chars in order
-          if(uart_data == 13)
-            uart_data <= 10;
-          else if(uart_data == 10)
+          if(uart_data == 10)
             uart_data <= 32;
           else if(uart_data == 126)
-            uart_data <= 13;
+            uart_data <= 10;
           else
             uart_data <= uart_data + 1;
-
+        
+        // increment tx state
         end else begin
           tx_state <= tx_state + 1;
         end
 
+      // transmit data bits
       end else if(tx_state > 0 && tx_state < 9) begin
 
         serial_txd <= uart_data[tx_state-1];
